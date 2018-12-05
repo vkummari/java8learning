34a4e197176b1276fd48d0c204ce00cd7f8e4c5f
Kummari, Venky (NIH/OD) [C] <venky.kummari@nih.gov>
2018-12-05 11:19:52 -0500

    First commit from the eclipse

--- src/main/java/com/java8learning/HelloWorld.java
+++ src/main/java/com/java8learning/HelloWorld.java
@@ -2,5 +2,9 @@ package com.java8learning;
 
 public class HelloWorld
 {
-	
+	public static void main(String[] args){
+		
+		System.out.println("Saying Hello World from my first java program in GitHub");
+		
+	}
 }
